c90ca7b9b2d2b584ccc3dc1ea011edf217416ad8
Anna Tkachenko <tkachenko.anna835@gmail.com>
2018-12-25 22:22:58 +0200

    Creates post entity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,16 @@ class Category
      * @ORM\Column(type="boolean")
      */
     private $isPublished;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Post", mappedBy="category", orphanRemoval=true)
+     */
+    private $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -71,4 +83,36 @@ class Category
 
         return $this;
     }
+
+    /**
+     * @return Collection|Post[]
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPosts(Post $posts): self
+    {
+        if (!$this->posts->contains($posts)) {
+            $this->posts[] = $posts;
+            $posts->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removePosts(Post $posts): self
+    {
+        if ($this->posts->contains($posts)) {
+            $this->posts->removeElement($posts);
+            // set the owning side to null (unless already changed)
+            if ($posts->getCategory() === $this) {
+                $posts->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
 }
